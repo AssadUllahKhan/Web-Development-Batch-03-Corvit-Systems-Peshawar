d50a8e3d7bb306cd91be3a77119523227c206d4b
Assad Ullah Khan <121865741+asadawan5556@users.noreply.github.com>
2025-10-29 23:42:14 +0500

    Added complete ClassWork and WebDev_Slides_and_Tasks folders containing HTML, CSS, and JavaScript lecture materials, slides, and tasks.
    This commit includes all classwork files (HTML, CSS, JavaScript) and the WebDev_Slides_and_Tasks folder containing lecture slides and student tasks for Web Development Batch 03 at Corvit Systems, Peshawar.

---
+++ ClassWork/JavaScript/Class2JS/script.js
@@ -0,0 +1,13 @@
+// function add(a, b) {
+//   c = a + b;
+//   return c;
+// }
+
+// const d = add(2, 4);
+// console.log(d);
+
+var add = function (a, b) {
+  return a + b;
+};
+var e = add(4, 5);
+console.log(e);
---
+++ ClassWork/JavaScript/class1JS/script.js
@@ -0,0 +1,46 @@
+console.log("Hello, World!");
+// alert("Hello, World!");
+document.getElementById("para").innerHTML = "Hello, World!";
+
+var greeting = "Hello!";
+// alert(greeting);
+
+const name = "Alice";
+
+a = "5";
+let b = [10, 20, 30, "assad", true];
+
+let school = {
+  roll: 1,
+  name: "Greenwood High",
+  location: "New York",
+  established: 1995,
+};
+console.log(school);
+
+c = 12;
+d = 13;
+e = c + d;
+document.write("The sum of c and d is: " + e);
+
+let f = "15";
+let g = 15;
+if (f == g && f === g) {
+  console.log("Both values and types are equal.");
+} else {
+  console.log("Values are equal but types are not.");
+}
+
+let marks = 85;
+console.log(marks >= 90);
+
+let names = ["Alice", "Bob", "Charlie"];
+console.log(names[0]);
+
+let students = {
+  roll: 101,
+  name: "David",
+  age: 20,
+  major: "Computer Science",
+};
+console.log(students.roll);
